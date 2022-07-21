469ad95f03684f5d29910e15ab508745abb9b502
Pedro Alegre Perrotta <pedroalegre29@gmail.com>
2022-07-21 12:11:04 -0300

    Sync

--- course-projects/01-starting-setup/src/App.js
+++ course-projects/01-starting-setup/src/App.js
@@ -1,4 +1,6 @@
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
+import ExpensesFilter from "./components/Expenses/ExpensesFilter";
 
 const App = () => {
   const expenses = [
@@ -23,9 +25,15 @@ const App = () => {
     },
   ];
 
+  const addExpenseHandler = expense => {
+    console.log('In App.js');
+    console.log(expense);
+  }
+
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <ExpensesFilter />
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
